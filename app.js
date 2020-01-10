4b97e91e33f98b99a02db8c4a86f633cd9aea3b5
Van Jacob Rebosura <rebosuravan@gmail.com>
2020-01-11 00:52:15 +0800

    dissapointment

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 
 // app specific variables
 let list = [];
+let work = [];
 let day;
 let dayOptions = {
   weekday: "long",
@@ -32,7 +33,7 @@ app.set('view engine', 'ejs');
 
 // server init
 app.listen(process.env.PORT || 3000, () => {
-  console.log("server running at port 3000");
+  console.log("server is now running");
 });
 
 // root route
@@ -44,10 +45,12 @@ app.get("/", (req, res) => {
   localeDate = day.toLocaleDateString("en-US", dayOptions);
 
   // renders 'view/index.ejs'
-  res.render('index', {
-    currentDate: localeDate,
-    todoList: list
-  });
+  // res.render('index', {
+  //   currentDate: localeDate,
+  //   todoList: list
+  // });
+
+  todoListRenderer("/", res);
 
 });
 
@@ -56,8 +59,42 @@ app.post("/", (req, res) => {
   let newListItem = req.body.newListItem;
 
   // add item to the list
-  list.push(newListItem);
+  let arr = addItem(newListItem, list);
 
   res.redirect("/");
 
-})
+});
+
+// test serve work route
+
+app.get('/work', (req, res) => {
+  todoListRenderer("work", res);
+});
+
+function addItem(listItem, itemArray) {
+  itemArray.push(listItem);
+  return itemArray;
+}
+
+function todoListRenderer(route, res) {
+
+  let title;
+  let currentList;
+
+  if(route === '/') {
+    localeDate = day.toLocaleDateString("en-US", dayOptions);
+    title = localeDate;
+    currentList = list;
+  } else {
+    title = route;
+    currentList = work;
+  }
+
+  let ejsVariables = {
+    listTitle: title,
+    todoList: currentList
+  }
+
+  res.render('index', ejsVariables);
+
+}
