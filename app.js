f80ef783f1963e0a40206ab1a7ff2b6420142b03
Van Jacob Rebosura <rebosuravan@gmail.com>
2020-01-18 23:18:25 +0800

    fix add entry functionality

--- app.js
+++ app.js
@@ -10,14 +10,7 @@ const mongoose = require('mongoose');
 // express init
 const app = express();
 
-// app specific variables
-let day;
-let dayOptions = {
-  weekday: "long",
-  month: "long",
-  day: "numeric"
-};
-let localeDate;
+
 
 
 // static folder
@@ -64,36 +57,46 @@ const itemSchema = {
 
 const Item = new mongoose.model('Item', itemSchema);
 
+// list schema
+
+const listSchema = {
+  name: {
+    type: String,
+    required: true
+  },
+  items: {
+    type: [itemSchema]
+  }
+};
+
+// list model
+
+const List = new mongoose.model('List', listSchema);
+
+
+// app specific variables
+
+let day;
+let dayOptions = {
+  weekday: "long",
+  month: "long",
+  day: "numeric"
+};
+let localeDate;
+
+const item0 = createItem('Hello and welcome to your list!');
+const item1 = createItem('Click the plus button to add a new entry!');
+const item2 = createItem('<--- Click here to cross off an entry!');
+
+let initialItems = [item0, item1, item2];
+
 // root route
 app.get("/", (req, res) => {
 
+  // redirect to default listTitle
 
-  // check if todolist has contents
+  res.redirect("/defaultList");
 
-  Item.find({}, (err, items) => {
-    if (items.length === 0) {
-
-      const item0 = createItem('Hello and welcome to your list!');
-      const item1 = createItem('Click the plus button to add a new entry!');
-      const item2 = createItem('<--- Click here to cross off an entry!');
-
-      let initialItems = [item0, item1, item2];
-
-      Item.insertMany(initialItems, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-
-    }
-
-      let ejsVariables = {
-        listTitle: "Today",
-        todoList: items
-      };
-
-      res.render('index', ejsVariables);
-  });
 
   // day = new Date();
   // converts date to displayable format
@@ -101,17 +104,97 @@ app.get("/", (req, res) => {
 
 });
 
+// default list route
+
+// app.get("/defaultList", (req, res) => {
+//     // check if todolist has contents
+//
+//     Item.find({}, (err, items) => {
+//       if (items.length === 0) {
+//
+//         Item.insertMany(initialItems, (err) => {
+//           if (err) {
+//             console.log(err);
+//           }
+//         });
+//
+//       }
+//
+//       let ejsVariables = {
+//         listId: "root",
+//         listTitle: "Today",
+//         todoList: items
+//       };
+//
+//       res.render('index', ejsVariables);
+//     });
+// });
+
+// custom list route
+
+app.get("/:customListName", (req, res) => {
+  let customListName = req.params.customListName;
+
+  if (customListName === "defaultList" ) {
+    customListName = "Today";
+  }
+
+  // check if list exists
+
+  List.findOne({
+    name: customListName
+  }, (err, list) => {
+    if (!err) {
+
+      if (!list) {
+
+        // push list to db
+
+
+
+        const newList = createList(customListName);
+        newList.save();
+
+        let ejsVariables = {
+          listId: newList._id,
+          listTitle: customListName,
+          todoList: initialItems
+        };
+
+        res.render('index', ejsVariables);
+      } else {
+
+        // redirect to existing list
+
+        let ejsVariables = {
+          listId: list._id,
+          listTitle: list.name,
+          todoList: list.items
+        };
+
+        res.render('index', ejsVariables);
+
+      }
+    }
+  });
+
+});
+
+
+
 // triggered when plus button is clicked
 app.post("/", (req, res) => {
 
-  let newListItem = req.body.newListItem;
+  const listId = req.body.listId;
+  const listName = req.body.listName;
+  const newListItem = req.body.newListItem;
+  const item = createItem(newListItem);
+  // item.save();
 
-  let item = createItem(newListItem);
-  item.save();
+  List.findOneAndUpdate({_id: listId}, {$push: {items: item}}, (err) => {
+    res.redirect("/" + listName);
+  });
 
-  let route = req.body.route;
-
-  res.redirect("/");
 
 });
 
@@ -121,7 +204,7 @@ app.post("/delete", (req, res) => {
   const itemId = (req.body.checkboxItem);
 
   Item.findOneAndDelete(itemId, (err) => {
-    if(!err) {
+    if (!err) {
       res.redirect("/");
     }
   });
@@ -131,5 +214,12 @@ app.post("/delete", (req, res) => {
 function createItem(name) {
   return new Item({
     name: name
+  });
+}
+
+function createList(name) {
+  return new List({
+    name: name,
+    items: initialItems
   });
 }
